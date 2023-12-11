1d5d0b478f11e90c146edaa2147af87fb8a08984
efdalyalcin <efdalyalcin@hotmail.com>
2023-12-11 20:56:44 +0100

    base arrange for deploy

--- vite.config.ts
+++ vite.config.ts
@@ -5,6 +5,7 @@ import react from '@vitejs/plugin-react';
 // https://vitejs.dev/config/
 export default defineConfig({
   plugins: [react()],
+  base: '/react_ts-aerospace-control-panel/',
   resolve: {
     alias: {
       '@': '/src',
